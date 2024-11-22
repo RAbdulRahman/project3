0f6995220618387a02dc62eba43fc25aa4778d6d
Rayhan Abdul Rahman <rabdulr4@uwo.ca>
2024-11-22 02:50:41 -0500

    initial testing
    testing plots with some arbitrary conditions close to what we will eventually get to.

--- AbdulRahman_RayhanMohammed_Project3.py
+++ AbdulRahman_RayhanMohammed_Project3.py
@@ -1,6 +1,7 @@
 #Project 3
 import numpy as np
 import scipy.integrate as scint
+import matplotlib.pyplot as plt
 
 
 # Question 1
@@ -13,11 +14,28 @@ import scipy.integrate as scint
 def deriv1(r,initstate): 
     rho = initstate[0]
     m = initstate[1]
-    x = rho**(1/3)
+    x = (rho)**(1/3)
     gamma = x**2/(3*(1+x**2)**0.5)
 
     d_rho = -m*rho/(gamma*r**2)
     d_mass = r**2 * rho
 
+    return d_rho,d_mass
 
+
+def stop_integ(r,state):
+    return state[1]
+
+stop_integ.terminal = True
+stop_integ.direction = -1
+
+r_span = np.linspace(1,7000,100000)
+initials = [0.1,0.1]
+
+test = scint.solve_ivp(deriv1,(1,7000),initials,t_eval =r_span,events=stop_integ)
+
+plt.plot(test.t,test.y[0])
+plt.show()
+plt.plot(test.t,test.y[1])
+plt.show()
 
